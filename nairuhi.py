146f9b8430119781b3898d7ca54afb82d2bdaaef
Narek Tovmasyan <nrk@amazon.com>
2019-03-30 00:06:59 -0700

    definitional implementational of Fib passes all 3 tests!

--- nairuhi.py
+++ nairuhi.py
@@ -4,15 +4,29 @@ import doctest
 
 
 def fib(n):
-	pass
+	"""
+	This is the block comment for the function!
+	>>> fib(1)
+	1
+	>>> fib(2)
+	1
+	>>> fib(3)
+	2
+	"""
+	if n == 1 or n == 2:
+		return 1
+	else:
+		return fib(n-1) + fib(n-2)
 
 def main():
 	"""
 	This is the doc-string for the main method!
 	"""
+	doctest.testmod() # puts in testing mode..
 	print("Hello World!")
-	res = fib(20)
-	print("Fibonacci function result:",res)
+	# n = 5
+	# res = fib(n)
+	# print("Fibonacci function result for n=%d is %d"%(n, res))
 
 if __name__ == '__main__':
 	main()
