563182e3e667039361215e31b2eef01aca9b102e
Narek Tovmasyan <nrk@amazon.com>
2019-03-29 23:45:25 -0700

    block comment added!

--- nairuhi.py
+++ nairuhi.py
@@ -1,10 +1,15 @@
 from __future__ import print_function
 
+import doctest
+
 
 def fib(n):
 	pass
 
 def main():
+	"""
+	This is the doc-string for the main method!
+	"""
 	print("Hello World!")
 	res = fib(20)
 	print("Fibonacci function result:",res)
